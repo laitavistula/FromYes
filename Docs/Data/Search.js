73ca81328c825568247d94aa9bfb9b6ac60562fc
Maria Laitalainen <maria.laitalainen@gmail.com>
2025-05-04 18:48:46 +0200

    Initial Check-in.

---
+++ Docs/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Introduction Topics/Disclaimer on inclusivity.htm'],s:['12','upominki'],p:[['12',6],['if',21],['the',17]]});
---
+++ Docs/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=638819805016091530'
+});
