2c2ca417c51d8ce261670c42fb0647ca422f5bc7
Maria Laitalainen <maria.laitalainen@gmail.com>
2025-05-13 18:08:13 +0200

    Changes to the TOC

--- Docs/Data/Search.js
+++ Docs/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Introduction Topics/Disclaimer on inclusivity.htm'],s:['1','procedur'],p:[['1',1],['dolor',36],['obtaining',10],['then',36]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A_Introduction Topics/Disclaimer on inclusivity.htm'],s:['1','procedur'],p:[['1',3],['don',26],['obywatela',31],['these',0]]});
--- Docs/Resources/Scripts/require.config.js
+++ Docs/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638827548494657912'
+    urlArgs: 't=638827564585939817'
 });
